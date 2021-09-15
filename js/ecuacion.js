739156fe0b3330ef02e725015a1ddfbc48a97eb7
Gonzoloso <ivanovichmaga@gmail.com>
2021-09-14 21:46:06 -0500

    Primer repositorio

---
+++ js/ecuacion.js
@@ -0,0 +1,18 @@
+var d; 
+var x1; 
+var x2; 
+var x; 
+var a = prompt("Introduce a:"); 
+var b = prompt("Introduce b:");
+var c = prompt("Introduce c:"); 
+var solucion1 =( -b+(Math . sqrt( (Math . pow(b, 2) - (4*a*c)) ) ) ) / (2*a) ; 
+var solucion2 = ( -b- (Math . sqrt( (Math . pow(b, 2) - (2*a*c)))))/(2*a) ;
+alert("Primera Solucion" + solucion1)
+alert("Segunda Solucion" + solucion2)
+console.log("Primera Solucon", Solucion1);
+alert("hola")
+
+
+d= b*b - 4 * a * c;
+    
+console.log(d)
